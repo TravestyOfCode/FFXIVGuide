5bfc3e5901ab10fd8770d7a8e3204b4764cbcf86
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-08 07:38:05 -0500

    Add project files.

---
+++ FFXIVGuide.Web/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FFXIVGuide.Web.Data;
+
+public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDBContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+}
---
+++ FFXIVGuide.Web/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FFXIVGuide.Web.Data;
+
+public class ApplicationUser : IdentityUser
+{
+
+}
