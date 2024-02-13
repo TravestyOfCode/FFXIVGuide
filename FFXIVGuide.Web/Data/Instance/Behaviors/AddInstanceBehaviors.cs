4773138e9833c563224358ec709e29d6139fa32d
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-12 23:31:43 -0500

    Moves validation behaviors before authorization behaviors.

--- FFXIVGuide.Web/Data/Instance/Behaviors/AddInstanceBehaviors.cs
+++ FFXIVGuide.Web/Data/Instance/Behaviors/AddInstanceBehaviors.cs
@@ -7,16 +7,16 @@ public static class InstanceBehaviorsExtensions
     public static MediatRServiceConfiguration AddInstanceBehaviors(this MediatRServiceConfiguration config)
     {
         // Create behaviors
-        config.AddBehavior<CreateInstanceAuthorization>(ServiceLifetime.Scoped);
         config.AddBehavior<CreateInstanceValidation>(ServiceLifetime.Scoped);
+        config.AddBehavior<CreateInstanceAuthorization>(ServiceLifetime.Scoped);
 
         // Update behaviors
-        config.AddBehavior<UpdateInstanceAuthorization>(ServiceLifetime.Scoped);
         config.AddBehavior<UpdateInstanceValidation>(ServiceLifetime.Scoped);
+        config.AddBehavior<UpdateInstanceAuthorization>(ServiceLifetime.Scoped);
 
         // Delete behaviors
-        config.AddBehavior<DeleteInstanceAuthorization>(ServiceLifetime.Scoped);
         config.AddBehavior<DeleteInstanceValidation>(ServiceLifetime.Scoped);
+        config.AddBehavior<DeleteInstanceAuthorization>(ServiceLifetime.Scoped);
 
         // Query behaviors
 
