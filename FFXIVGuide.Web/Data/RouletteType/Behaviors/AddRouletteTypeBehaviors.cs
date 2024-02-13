ab171cf6f6652ea621ab8652aae4350493293e2b
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-13 17:17:23 -0500

    Moves Validation before Authorization in behavior pipeline.

--- FFXIVGuide.Web/Data/RouletteType/Behaviors/AddRouletteTypeBehaviors.cs
+++ FFXIVGuide.Web/Data/RouletteType/Behaviors/AddRouletteTypeBehaviors.cs
@@ -7,15 +7,15 @@ public static class RouletteTypeBehaviorsExtensions
     public static MediatRServiceConfiguration AddRouletteTypeBehaviors(this MediatRServiceConfiguration config)
     {
         // Create Behaviors
-        config.AddBehavior<CreateRouletteTypeAuthorization>(ServiceLifetime.Scoped);
         config.AddBehavior<CreateRouletteTypeValidation>(ServiceLifetime.Scoped);
+        config.AddBehavior<CreateRouletteTypeAuthorization>(ServiceLifetime.Scoped);
 
         // Update Behaviors
-        config.AddBehavior<UpdateRouletteTypeAuthorization>(ServiceLifetime.Scoped);
+        config.AddBehavior<UpdateRouletteTypeValidation>(ServiceLifetime.Scoped);
         config.AddBehavior<UpdateRouletteTypeAuthorization>(ServiceLifetime.Scoped);
 
         // Delete Behaviors
-        config.AddBehavior<DeleteRouletteTypeAuthorization>(ServiceLifetime.Scoped);
+        config.AddBehavior<DeleteRouletteTypeValidation>(ServiceLifetime.Scoped);
         config.AddBehavior<DeleteRouletteTypeAuthorization>(ServiceLifetime.Scoped);
 
         // Query Behaviors
