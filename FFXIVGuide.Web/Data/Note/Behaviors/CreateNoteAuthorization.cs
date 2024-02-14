9bc7656840fdda8e1fd4703347d9f7267a2dc4a1
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-13 20:12:44 -0500

    Implements Authorization behavior.

--- FFXIVGuide.Web/Data/Note/Behaviors/CreateNoteAuthorization.cs
+++ FFXIVGuide.Web/Data/Note/Behaviors/CreateNoteAuthorization.cs
@@ -1,4 +1,6 @@
 ﻿using FFXIVGuide.Web.Data.Note.Commands;
+using FFXIVGuide.Web.Services;
+using System.Security.Claims;
 
 namespace FFXIVGuide.Web.Data.Note.Behaviors;
 
@@ -6,16 +8,31 @@ public class CreateNoteAuthorization : IPipelineBehavior<CreateNote, Result<Note
 {
     private readonly ILogger<CreateNoteAuthorization> _logger;
 
-    public CreateNoteAuthorization(ILogger<CreateNoteAuthorization> logger)
+    private readonly IUserAccessor _user;
+
+    public CreateNoteAuthorization(ILogger<CreateNoteAuthorization> logger, IUserAccessor user)
     {
         _logger = logger;
+        _user = user;
     }
 
     public async Task<Result<NoteModel>> Handle(CreateNote request, RequestHandlerDelegate<Result<NoteModel>> next, CancellationToken cancellationToken)
     {
         try
         {
-            // TODO: Only an Admin role user will be able to create new Notes
+            // User can create notes if themselves
+            // Admin can create notes for any user
+            if (!_user.CurrentUser.IsInRole("Admin"))
+            {
+                var userId = _user.CurrentUser.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                ArgumentNullException.ThrowIfNull(userId);
+
+                if (!request.OwnerId.Equals(userId))
+                {
+                    return Result.Forbidden<NoteModel>();
+                }
+            }
             return await next();
         }
         catch (Exception ex)
