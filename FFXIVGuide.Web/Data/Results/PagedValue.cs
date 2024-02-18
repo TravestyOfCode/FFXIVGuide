fa734836f38ed6a64579b115bb84e8d89499e29f
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-18 13:17:24 -0500

    Adds PagedValue<T> object to hold page information and enumerable of <T>.

---
+++ FFXIVGuide.Web/Data/Results/PagedValue.cs
@@ -0,0 +1,26 @@
+﻿namespace FFXIVGuide.Web.Data.Results;
+
+public class PagedValue<T> : IPagedInfo
+{
+    public double TotalPages { get; set; }
+
+    public int Page { get; set; }
+
+    public int PerPage { get; set; }
+
+    public string SortBy { get; set; }
+
+    public SortOrder SortOrder { get; set; }
+
+    public IEnumerable<T> Values { get; set; }
+
+    public PagedValue()
+    {
+        TotalPages = 0;
+        Page = 1;
+        PerPage = 10;
+        SortBy = string.Empty;
+        SortOrder = SortOrder.ASC;
+        Values = new List<T>();
+    }
+}
