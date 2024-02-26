4567fb38436bd6eb04b7d702220e168e26af0a7c
travis@travestyofcode.com <travis@travestyofcode.com>
2024-02-26 08:48:55 -0500

    Adds Utilities for string constants.

---
+++ FFXIVGuide.Web/Utilities/ViewUtils.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FFXIVGuide.Web.Utilities;
+
+public static class ViewUtils
+{
+    public static bool IsHXRequest(this HttpRequest request) => request.Headers.ContainsKey("hx-request");
+}
+
+public static class ViewNames
+{
+    public static readonly string DataList = "DataList";
+    public static readonly string Index = "Index";
+    public static readonly string EditingRow = "EditingRow";
+    public static readonly string EditableRow = "EditableRow";
+}
