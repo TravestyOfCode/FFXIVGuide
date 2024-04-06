ce4deb7fd07b20816b073018e93b7fa96591748b
travis@travestyofcode.com <travis@travestyofcode.com>
2024-04-06 12:46:15 -0400

    Creates RouletteType controller.

---
+++ FFXIVGuideAPI/Controllers/RouletteTypeController.cs
@@ -0,0 +1,25 @@
+﻿using FFXIVGuideAPI.Models.RouletteType.Commands;
+using FluentResults.Extensions.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FFXIVGuideAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RouletteTypeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RouletteTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateRouletteType request)
+    {
+        return (await _mediator.Send(request)).ToActionResult();
+    }
+}
