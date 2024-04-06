4c59487e0f3e6d36dab6d07e92eb74e49671a597
travis@travestyofcode.com <travis@travestyofcode.com>
2024-04-06 12:45:15 -0400

    Adds RouletteType DTO.

---
+++ FFXIVGuideAPI/Models/RouletteType/RouletteType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FFXIVGuideAPI.Models.RouletteType;
+
+public class RouletteType
+{
+    /// <summary>
+    /// The unique identifier for the RouletteType
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The display name of the RouletteType. Must be unique.
+    /// </summary>
+    [Required]
+    [MaxLength(64)]
+    public string Name { get; set; }
+
+    public RouletteType()
+    {
+        Name = string.Empty;
+    }
+}
